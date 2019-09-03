a36e03fbe2e0d15c41340a9a48800b6d8bdc4a29
danialdo95 <danialdo95@gmail.com>
2019-09-04 06:38:24 +0800

    Added Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a recipe', 'https://assets.bonappetit.com/photos/57ae3e611b33404414975c0d/1:1/w_414,h_414,c_limit/roasted-chicken-thighs-with-lemon-and-oregano.jpg')
+  ];
 
   constructor() { }
 
